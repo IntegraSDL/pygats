ba154c22dc205dd9f704369588088f5096fdcd93
Sysoev, Vladimir <vladimir.sysoev@sarov-itc.ru>
2023-01-26 14:39:20 +0300

    Fixed #21 moved markdown to separate module

--- tests/test_pygats.py
+++ tests/test_pygats.py
@@ -1,30 +1,28 @@
 """Module with library tests"""
 import pytest
-from pygats.pygats import Context, begin_test, MarkdownFormatter, step, check
+import os
+from pygats.pygats import Context, screenshot 
+from pygats.formatters import MarkdownFormatter as MD
 #from PIL import Image
 
+def setup_module(module):
+    try:
+        os.mkdir('output')
+    except FileExistsError:
+        pass
 @pytest.fixture
 def formatter_fixture():
     """formatter fixture for markdown"""
-    return MarkdownFormatter()
+    return MD()
 
 
-def test_formatter(formatter_fixture, capsys): # pylint: disable=redefined-outer-name
-    """Test formatter"""
+def test_screenshot(formatter_fixture, capsys):
+    """test screenshot"""
     ctx = Context(formatter_fixture)
     assert ctx
-    begin_test(ctx, 'First message')
+    screenshot(ctx)
     cptrd = capsys.readouterr()
-    assert cptrd.out == '\n### First message\n\n'
-
-    step(ctx, 'Hello world')
-    cptrd = capsys.readouterr()
-    assert cptrd.out == '\nStep 1: Hello world\n\n'
+    print(cptrd.out)
+    assert cptrd.out == '![Screenshot](step-1-0-passed.png)\n\n'
 
 
-def test_check(formatter_fixture): # pylint: disable=redefined-outer-name
-    """Test check function"""
-    ctx = Context(formatter_fixture)
-    assert ctx
-    result = check(ctx, 'Hello world', None)
-    assert result is None
