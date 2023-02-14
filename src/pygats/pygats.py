234f9e24b1efb977c86f208ffd603de67246ad34
Maxim Korotkov <korotkov.maxim.s@gmail.com>
2023-02-14 17:57:33 +0300

    remove  Trailing whitespace

--- src/pygats/pygats.py
+++ src/pygats/pygats.py
@@ -447,7 +447,7 @@ def combine_lines(lines):
 
 def findText(img, text, lang, skip=0):
     """Function finds text in image with Tesseract"""
-    #TODO: Refactor this function to reduce its Cognitive 
+    #TODO: Refactor this function to reduce its Cognitive
     # Complexity from 20 to the 15 allowed. sonarlint(python:S3776)
     recognized = pytesseract.image_to_data(img, lang).split('\n')
     combine_words_in_lines(recognized)
