1e0a667440b54055a8a8a1d6c25d2789fbe13e27
Sysoev, Vladimir <vladimir.sysoev@sarov-itc.ru>
2023-03-02 12:14:23 +0300

    fixed pylint issue

--- src/pygats/__init__.py
+++ src/pygats/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ["formatter", "pygats", "misc"]
+"""
+pyGATs tis python3 library which combines power of pyautogui, opencv,
+tesseract, markdown and other staff to automate end-to-end and exploratory
+testing.
+"""
+
+__all__ = ["formatters", "pygats", "misc"]
