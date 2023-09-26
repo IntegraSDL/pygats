6a0995be49ac5d2377b3682fcfcc029db21c2d46
Maxim Korotkov <korotkov.maxim.s@gmail.com>
2023-09-26 11:42:08 +0300

    pygats: added type hints
    Signed-off-by: Maxim Korotkov <korotkov.maxim.s@gmail.com>

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -3,13 +3,14 @@ import pytest
 from pygats.pygats import Context, begin_test, step, check, suite
 from pygats.formatters import MarkdownFormatter as MD
 
+
 @pytest.fixture(name='formatter')
 def fixture_formatter():
     """formatter fixture for markdown"""
     return MD()
 
 
-def test_formatter(formatter, capsys): # pylint: disable=redefined-outer-name
+def test_formatter(formatter, capsys):  # pylint: disable=redefined-outer-name
     """Test formatter"""
     ctx = Context(formatter)
     assert ctx
@@ -22,7 +23,7 @@ def test_formatter(formatter, capsys): # pylint: disable=redefined-outer-name
     assert cptrd.out == '\nStep 1: Hello world\n\n'
 
 
-def test_check(formatter, capsys): # pylint: disable=redefined-outer-name
+def test_check(formatter, capsys):  # pylint: disable=redefined-outer-name
     """Test check function"""
     ctx = Context(formatter)
     assert ctx
@@ -36,6 +37,6 @@ def test_suite(formatter, capsys):
     """Test suite function"""
     ctx = Context(formatter)
     assert ctx
-    suite(ctx, 'suite name','Hello world')
+    suite(ctx, 'suite name', 'Hello world')
     cptrd = capsys.readouterr()
     assert cptrd.out == '\n\n## Hello world\n\n'
