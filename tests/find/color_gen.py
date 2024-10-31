6e1a0873cf0c4f472340f1914b3082d96b803444
Иван Горбунов <muervos@muervos.muervos>
2024-11-01 02:11:55 +0300

    Add color generator and try to generate pictures with different color fills

---
+++ tests/find/color_gen.py
@@ -0,0 +1,8 @@
+def color_generator():
+    rgb = []
+    for r in range(256):
+        for g in range(256):
+            for b in range(256):
+                color = f"#{r:02x}{g:02x}{b:02x}"
+                rgb.append(color)
+    return rgb
--- tests/find/gen.py
+++ tests/find/gen.py
@@ -1,9 +1,24 @@
+import time
 from PIL import Image, ImageDraw, ImageFont
+import color_gen
 
 
 def crop_image(img, w, h):
     img_crop = img.crop((0, 0, w, h))
     return img_crop
+
+
+def wrapper():
+    start = time.time()
+    gen("blue", 350, 350, 'Arial_Bold', 150, "File")
+    seconds = int(time.time() - start)
+    seconds = seconds % (24 * 3600)
+    hours = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    with open('tests/find/result.txt', 'a') as file:
+        file.write('Время выполнения: %02d:%02d:%02d\n' % (hours, minutes, seconds))
 
 
 def gen(filename, w, h, font='', size=16, text='', crop=False):
@@ -13,7 +28,11 @@ def gen(filename, w, h, font='', size=16, text='', crop=False):
         img = crop_image(img, 350, 50)
     font = ImageFont.truetype(f'tests/find/fonts/{font}.ttf', size=size)
     draw_text = ImageDraw.Draw(img)
-    draw_text.text((w, h), text, font=font, fill=('#000000'))
-    img.save('tests/find/1.jpg')
-    img = Image.open('tests/find/1.jpg')
+    colors = color_gen.color_generator()
+    for fill_color in colors:
+        draw_text.text((w, h), text, font=font, fill=fill_color)
+        img.save(f'tests/find/font_colors/{fill_color[1:]}.jpg')
+    #img = Image.open('tests/find/1.jpg')
     return img
+
+wrapper()
