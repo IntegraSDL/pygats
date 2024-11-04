313041863bb37b13396f8ac2649e49227c213769
Иван Горбунов <muervos@muervos.muervos>
2024-11-04 17:22:45 +0300

    clean up

--- tests/test_recog.py
+++ tests/test_recog.py
@@ -20,30 +20,6 @@ def fixture_create_ctx(formatter: MD):
     ctx = pyg.Context(formatter)
     return ctx
 
-def test_check_text(capsys, generator_photo):
-    """test check_text checks if "File is exist on image"""
-    text = rec.SearchedText("File", "eng", None)
-    width, height = img.size
-    assert width > 0
-    assert height > 0
-    fill_color = Path('find/fill_colors')
-    for picture in fill_color.glob('*'):
-        img = Image.open(f'tests/find/fill_colors/{fill_color[1:]}.jpg')
-        print(f"Проверка картинки {picture}с цветом шрифта {fill_color[1:]}")
-        rec.check_text(ctx, img, text)
-        cptrd = capsys.readouterr()
-        assert '![Успешно](step-1-passed.png)\n\n**Успешно**\n\n' in cptrd.out
-
-
-def test_check_text_failed(capsys, generator_photo):
-    """test check_text with word Fie which tesseract cant find"""
-    text = rec.SearchedText("Fie", "eng", None)
-    img = generator_photo
-    with pytest.raises(pyg.TestException):
-        rec.check_text(ctx, img, text)
-        cptrd = capsys.readouterr()
-        assert cptrd == f"{text} не найден на изображении"
-
 
 def test_check_text_1(capsys):
     text = rec.SearchedText("File", "eng", None)
