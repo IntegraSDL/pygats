2685ea73866b9a08dc57a7a3fb0c2b6c09141514
Иван Горбунов <muervos@muervos.muervos>
2024-11-04 17:22:45 +0300

    clean up

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-import tests.find.gen as generator
-
-@pytest.fixture(name='generator_photo', scope="function")
-def gen_photo():
-    return generator.gen("blue", 350, 350, 'Arial_Bold', 150, "File")
