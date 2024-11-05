f43c5df0547cd8193bc5cd6ae5a7bb6672426ee1
Иван Горбунов <muervos@muervos.muervos>
2024-11-05 23:27:03 +0300

    made an example of work bg_changer and check_text for future histogram

--- tests/test_recog.py
+++ tests/test_recog.py
@@ -4,7 +4,7 @@ import pytest
 import pygats.recog as rec
 import pygats.pygats as pyg
 from pygats.formatters import MarkdownFormatter as MD
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from tests.find import gen
 
 @pytest.fixture(name='formatter', scope="session", autouse=True)
@@ -21,9 +21,44 @@ def fixture_create_ctx(formatter: MD):
     return ctx
 
 
-def test_check_text_1(capsys):
-    text = rec.SearchedText("File", "eng", None)
+@pytest.fixture(scope="function")
+def words_for_bg():
     gen.bg_changer(True)
-    rec.check_text(ctx, "tests/find/1.jpg", text)
-    cptrd = capsys.readouterr()
-    assert '![Успешно](step-1-passed.png)\n\n**Успешно**\n\n' in cptrd.out
+    file = open("tests/find/words.txt")
+    lines = file.readlines()
+    font = ImageFont.truetype(f'tests/find/fonts/Arial_Bold.ttf', size=60)
+    texts = [] 
+    for line in lines:
+        if not line.strip():
+            continue
+        img = Image.open('tests/find/2.jpg')
+        draw_text = ImageDraw.Draw(img)
+        draw_text.text((50, 50),line.strip() , font=font, fill="#000000")
+        text = rec.SearchedText(line.strip(), "eng", None)
+        texts.append(text)
+        img.save(f'tests/find/fill_colors/{line.strip()}.jpg', quality=95)
+    print(texts)
+    return texts 
+  
+        
+def test_check_text_1(capsys, words_for_bg):
+    successful_count = 0
+    failed_count = 0
+    fill_color = Path('tests/find/fill_colors')
+    for expected_text in words_for_bg:
+        image_path = fill_color / f"{expected_text.content}.jpg"
+        print(f"Проверяем файл: {image_path}") 
+        if image_path.exists():
+            img = Image.open(image_path)
+            result = rec.check_text(ctx, img, expected_text)
+            print(f"Файл {image_path} найден.")
+            if result:
+                successful_count += 1
+            else:
+                failed_count += 1
+        else:
+            print(f"Файл {image_path} не найден.")
+            failed_count += 1
+            
+    print(f"Успешно распознанных слов: {successful_count}")
+    print(f"Не распознанных слов: {failed_count}")
