8f02d2c8801553b9a52ff6721480b5b6afe7403d
Иван Горбунов <muervos@muervos.muervos>
2024-11-15 22:06:28 +0300

    Create a color shade generator and test check_text
    Create the color_shade_gen function in order to investigate the dependence of the tesseract recognition quality on the background color and make a test for the check_text function, which uses a library with a built-in neural network
    
    Built a histogram where display the results of text recognition in different shades of color
    
    Conclusion: the accuracy of text recognition in different background colors depends on contrast

--- tests/find/gen.py
+++ tests/find/gen.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import time
 from PIL import Image, ImageDraw, ImageFont
 
@@ -34,32 +35,55 @@ def gen(filename, w, h, font='', size=16, text='', crop=False):
         img.save(f'tests/find/fill_colors/{fill_color[1:]}.jpg', quality=95)
     return img
 
-def bg_changer(crop=False):
-    img = Image.open('tests/find/1.jpg')
+
+def color_shade_gen(rgb: tuple, step: int, size: tuple = (1920, 1080), crop=False):
+    """Function generates images with different shades of color
+
+    Args:
+        rgb (list): List of color values in the rgb color model
+        step (int): Step that determines the frequency of creating shades of color
+        size (tuple): Specifying the size of the image to be created
+        crop (bool, optional): Parameter responsible for cropping the image Defaults to False.
+    """
+    new_image = Image.new('RGB', size, rgb)
+    new_image.save("tests/find/new_image.png", 'PNG')
+    img = Image.open("tests/find/new_image.png")
+    folder_path = Path(f'tests/find/color_shades')
+    folder_path.mkdir(parents=True, exist_ok=True)
     if crop:
         img = crop_image(img, 350, 350)
     img = img.convert("RGB")
     datas = img.getdata()
-    new_image_data = []
-    for item in datas:
-        if item[0]>200 and item[1]<80 and item[2]<80:
-            new_image_data.append((255, 0, 0)) # 1. Красный цвет
-        elif item[1] > 200 and item[0] < 80 and item[2] < 80:
-            new_image_data.append((0, 255, 0)) # 2. Зеленый цвет
-        elif item[2] > 200 and item[0] < 80 and item[1] < 80:
-            new_image_data.append((0, 0, 255)) # 3. Синий цвет
-        elif item[0] > 200 and item[1] > 200 and item[2] < 80:
-            new_image_data.append((255, 255, 0)) # 4. Желтый цвет
-        elif item[1] > 150 and item[2] > 200 and item[0] < 80:
-            new_image_data.append((66, 170, 255)) # 5. Голубой цвет
-        elif item[0] > 100 and item[2] > 200 and item[1] < 80:
-            new_image_data.append((139,0,255)) # 6. Фиолетовый цвет
-        elif item[0] > 200 and item[1] > 200 and item[2] > 200:
-            new_image_data.append((255, 255, 255)) # 7. Белый цвет
-        elif item[0] < 80 and item[1] < 80 and item[2] < 80:
-            new_image_data.append((0, 0, 0)) # 8. Черный цвет
-        else:
-            new_image_data.append(item)
-    img.putdata(new_image_data)
-    img.save("tests/find/2.jpg", 'JPEG')
-    # img.show()
+    for color in range(0, 256, step):
+        new_image_data = []
+        for item in datas:
+            if item[0]>200 and item[1]<80 and item[2]<80:
+                new_image_data.append((color, 0, 0)) # Red color
+            elif item[1] > 200 and item[0] < 80 and item[2] < 80:
+                new_image_data.append((0, color, 0)) # Green color
+            elif item[2] > 200 and item[0] < 80 and item[1] < 80:
+                new_image_data.append((0, 0, color)) # Blue color
+        new_img = Image.new('RGB', img.size)
+        new_img.putdata(new_image_data)
+        new_img.save(f"tests/find/color_shades/color{color}.png", 'PNG')
+
+color_shade_gen((210, 0, 0), 5, (1280,1280))
+
+
+        # if item[0]>200 and item[1]<80 and item[2]<80:
+        #     new_image_data.append((255, 0, 0)) # 1. Красный цвет
+        # elif item[1] > 200 and item[0] < 80 and item[2] < 80:
+        #     new_image_data.append((0, 255, 0)) # 2. Зеленый цвет
+
+        # elif item[0] > 200 and item[1] > 200 and item[2] < 80:
+        #     new_image_data.append((255, 255, 0)) # 4. Желтый цвет
+        # elif item[1] > 150 and item[2] > 200 and item[0] < 80:
+        #     new_image_data.append((66, 170, 255)) # 5. Голубой цвет
+        # elif item[0] > 100 and item[2] > 200 and item[1] < 80:
+        #     new_image_data.append((139,0,255)) # 6. Фиолетовый цвет
+        # elif item[0] > 200 and item[1] > 200 and item[2] > 200:
+        #     new_image_data.append((255, 255, 255)) # 7. Белый цвет
+        # elif item[0] < 80 and item[1] < 80 and item[2] < 80:
+        #     new_image_data.append((0, 0, 0)) # 8. Черный цвет
+        # else:
+        #     new_image_data.append(item)
--- tests/test_recog.py
+++ tests/test_recog.py
@@ -23,43 +23,67 @@ def fixture_create_ctx(formatter: MD):
 
 @pytest.fixture(scope="function")
 def words_for_bg():
-    gen.bg_changer(True)
+    gen.color_shade_gen((210, 0, 0), 5, (1280,1280), True)
     file = open("tests/find/words.txt")
     lines = file.readlines()
-    font = ImageFont.truetype(f'tests/find/fonts/Arial_Bold.ttf', size=60)
+    font = ImageFont.truetype(f'tests/find/fonts/Arial_Bold.ttf', size=27)
     texts = [] 
-    for line in lines:
-        if not line.strip():
-            continue
-        img = Image.open('tests/find/2.jpg')
-        draw_text = ImageDraw.Draw(img)
-        draw_text.text((50, 50),line.strip() , font=font, fill="#000000")
-        text = rec.SearchedText(line.strip(), "eng", None)
-        texts.append(text)
-        img.save(f'tests/find/fill_colors/{line.strip()}.jpg', quality=95)
-    print(texts)
-    return texts 
+    colors = Path('tests/find/color_shades')
+    colors_and_texts = Path(f'tests/find/fill_colors')
+    colors_and_texts.mkdir(parents=True, exist_ok=True)
+    for photo in colors.glob('*'):
+        img = Image.open(photo)
+        for line in lines:
+            if not line.strip():
+                continue
+            new_img = img.copy()
+            draw_text = ImageDraw.Draw(new_img)
+            draw_text.text((100, 100),line.strip(), font=font, fill="#000000")
+            text = rec.SearchedText(line.strip(), "eng", None)
+            texts.append(text)
+            subfolder_name = photo.name[:8]
+            folder_path = Path(f'tests/find/fill_colors/{subfolder_name}')
+            folder_path.mkdir(parents=True, exist_ok=True)
+            new_img.save(f'tests/find/fill_colors/{subfolder_name}/{line.strip()}.png', quality=100)
+    return texts
   
-        
-def test_check_text_1(capsys, words_for_bg):
+
+def test_check_text_1(words_for_bg, capsys):
     successful_count = 0
     failed_count = 0
-    fill_color = Path('tests/find/fill_colors')
-    for expected_text in words_for_bg:
-        image_path = fill_color / f"{expected_text.content}.jpg"
-        print(f"Проверяем файл: {image_path}") 
-        try:
-            if image_path.exists():
-                img = Image.open(image_path)
-                result = rec.check_text(ctx, img, expected_text)
-                print(f"Файл {image_path} найден.")
-                if result:
-                    successful_count += 1
-                else:
+    fill_color = Path(f'tests/find/fill_colors')
+    for folders in fill_color.glob('*'):
+        image_count = 0
+        assert folders.is_dir()
+        for expected_text in words_for_bg:
+            image_path = folders / f"{expected_text.content}.png"
+            print(f"Проверяем файл: {image_path}")
+            image_count += 1 
+            try:
+                if image_path.exists():
+                    img = Image.open(image_path)
+                    width, height = img.size
+                    assert width > 0
+                    assert height > 0
+                    result = rec.check_text(ctx, img, expected_text)
+                    cptrd = capsys.readouterr()
+                    print(f"Файл {image_path} найден.")
+                    if '![Успешно]' in cptrd.out:
+                        successful_count += 1
+                    else:
+                        failed_count += 1
+                        assert cptrd == f"{expected_text} не найден на изображении"
+            except pyg.TestException:
+                    print(f"Файл {image_path} не найден.")
                     failed_count += 1
-        except pyg.TestException:
-                print(f"Файл {image_path} не найден.")
-                failed_count += 1
-            
-    print(f"Успешно распознанных слов: {successful_count}")
-    print(f"Не распознанных слов: {failed_count}")
+            if image_count >= 100:
+                with open('tests/find/result.txt', 'a') as file:
+                    file.write(f"Папка: {folders}\n")
+                    file.write(f"Успешно распознанных слов: {successful_count} в {folders}\n")
+                    file.write(f"Не распознанных слов: {failed_count} в {folders}\n")
+                    file.write(f"---------------------------------\n")
+                successful_count = 0
+                failed_count = 0
+                assert successful_count == 0
+                assert failed_count == 0
+                break
