723c406eca29c08cbd6203736b355474f59134cc
Иван Горбунов <muervos@muervos.muervos>
2024-12-08 18:13:43 +0300

    research  graphics

--- tests/find/histogram.py
+++ tests/find/histogram.py
@@ -1,30 +1,28 @@
 import matplotlib.pyplot as plt
 
-colors = ['#000000', '#000500', '#000A00', '#000F00', '#00FF00']
+colors = ['#FEFEFE', '#FDFFF7', '#FCFFFA', '#FBFFFD']
 
 bar_width = 0.3
 x = range(len(colors))
 fig, ax = plt.subplots()
 ax.set_facecolor('lightblue')
 
-plt.bar(0, 0, width=bar_width, color='#000000')
-plt.bar(0 + bar_width, 100, width=bar_width, label='Не распознанно', color='gray')
+plt.bar(0, 89, width=bar_width, color='#FEFEFE')
+plt.bar(0 + bar_width, 11, width=bar_width, color='gray')
 
-plt.bar(1, 100, width=bar_width, color='#000500')
+plt.bar(1, 99, width=bar_width, color='#FDFFF7')
 plt.bar(1 + bar_width, 0, width=bar_width, color='gray')
 
-plt.bar(2, 100, width=bar_width, color='#000A00')
+plt.bar(2, 100, width=bar_width, color='#FCFFFA')
 plt.bar(2 + bar_width, 0, width=bar_width, color='gray')
 
-plt.bar(3, 100, width=bar_width, color='#000F00')
-plt.bar(3 + bar_width, 0, width=bar_width, color='gray')
+plt.bar(3, 98, width=bar_width, color='#FBFFFD')
+plt.bar(3 + bar_width, 2, width=bar_width, label='Не распознанно', color='gray')
 
-plt.bar(4, 100, width=bar_width, color='#00FF00')
-plt.bar(4 + bar_width, 0, width=bar_width, color='gray')
 
 plt.xlabel('Цвета')
 plt.ylabel('Количество')
-plt.title('Распознанные и нераспознанные цвета')
+plt.title('Качество распознававния от контрастности 1.0039')
 plt.xticks([i + bar_width / 2 for i in x], colors)
 
 plt.legend()
