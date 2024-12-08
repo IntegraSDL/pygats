0f206657338cfbbe466f088dc892e99abbbdd541
Иван Горбунов <muervos@muervos.muervos>
2024-12-08 20:41:09 +0300

    clean up

--- src/locales/find_locale.py
+++ src/locales/find_locale.py
@@ -2,6 +2,7 @@ import locale
 import locales.eng.eng as eng
 import locales.rus.rus as rus
 
+
 def answer(msg):
     lang = locale.getlocale()
     if lang[0] == "ru_RU":
@@ -10,5 +11,6 @@ def answer(msg):
     elif lang[0] == "en_US":
         print(eng.message[msg])
         return eng.message[msg]
-    
+
+
 answer("move_to_text_pos")
