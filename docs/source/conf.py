719cf07e93ff9f02170b5a1d288e1754cb0ffa86
Sysoev, Vladimir <i@vsysoev.ru>
2025-02-19 23:58:05 +0300

    playing with path to src of pygats

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,9 +5,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+from pathlib import Path
+sys.path.insert(0, str(Path('../..', 'src').resolve()))
 
 project = 'pyGATs'
 copyright = '2025, Vladimir Sysoev'
