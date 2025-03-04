d4916cd5fadb81aab891986443e2136734dd0e1c
Иван Горбунов <muervos@muervos.muervos>
2025-03-04 22:48:04 +0300

    fix path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 from pathlib import Path
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('./src'))
 
 project = 'pyGATs'
 copyright = '2025, Vladimir Sysoev'
