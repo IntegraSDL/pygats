898953830de3784e62fac2a71c89e703b583c641
Иван Горбунов <muervos@muervos.muervos>
2025-03-04 23:57:38 +0300

    copy last branch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,22 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+from pathlib import Path
+sys.path.insert(0, os.path.abspath('../../src'))
+
 project = 'pyGATs'
-copyright = '2023, Vladimir Sysoev'
+copyright = '2025, Vladimir Sysoev'
 author = 'Vladimir Sysoev'
+release = '0.1.0'
 
-release = '0.0'
-version = '0.1.0'
-
-# -- General configuration
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.duration',
@@ -17,17 +28,14 @@ extensions = [
     'sphinx.ext.todo'
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
 templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output
+root_doc = 'index'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+html_static_path = ['_static']
