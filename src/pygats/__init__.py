6f57ac471c01598ac336569ae930f2938ad0f1b3
Иван Горбунов <muervos@muervos.muervos>
2025-03-04 21:36:58 +0300

    remove imports init

--- src/pygats/__init__.py
+++ src/pygats/__init__.py
@@ -3,5 +3,4 @@ pyGATs tis python3 library which combines power of pyautogui, opencv,
 tesseract, markdown and other staff to automate end-to-end and exploratory
 testing.
 """
-import formatters, pygats, misc, recog
 __all__ = ["formatters", "pygats", "misc", "recog"]
