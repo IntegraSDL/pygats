923d2072f2bc3d95931254d7e98ec92348026623
Иван Горбунов <muervos@muervos.muervos>
2025-03-05 00:03:43 +0300

    fix init file

--- src/pygats/__init__.py
+++ src/pygats/__init__.py
@@ -3,5 +3,9 @@ pyGATs tis python3 library which combines power of pyautogui, opencv,
 tesseract, markdown and other staff to automate end-to-end and exploratory
 testing.
 """
+import pygats.recog
+import pygats.formatters
+import pygats.misc
+import pygats.pygats
 
-__all__ = ["formatters", "pygats", "misc"]
+__all__ = ["formatters", "pygats", "misc", "recog"]
