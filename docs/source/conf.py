c33ed4f02436b32739bafffa828f6b959c263081
Иван Горбунов <muervos@muervos.muervos>
2025-03-06 00:00:31 +0300

    remove double pygat

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 from pathlib import Path
-sys.path.insert(0, os.path.abspath('../../src/'))
+sys.path.insert(0, os.path.abspath('../../pygats/'))
 
 autodoc_mock_imports = ['pyautogui', 'mouseinfo']
 
