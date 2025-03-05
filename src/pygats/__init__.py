09bdf6b8f96ec3dc06d2041d68c29451a6f3acd6
Иван Горбунов <muervos@muervos.muervos>
2025-03-06 01:09:03 +0300

    add functions in init

--- src/pygats/__init__.py
+++ src/pygats/__init__.py
@@ -3,4 +3,5 @@ pyGATs tis python3 library which combines power of pyautogui, opencv,
 tesseract, markdown and other staff to automate end-to-end and exploratory
 testing.
 """
-__all__ = ["formatters", "pygats", "misc", "recog"]
+from .pygats import step, passed, failed
+__all__ = ["alt_with_key", "passed", "step", "passed", "failed","formatters", "pygats", "misc", "recog"]
