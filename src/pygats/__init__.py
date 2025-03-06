85bea49d311efdc93f76f620127eccd5d77e3610
Иван Горбунов <muervos@muervos.muervos>
2025-03-06 09:44:49 +0300

    add whitespace for init.py in pygats

--- src/pygats/__init__.py
+++ src/pygats/__init__.py
@@ -3,4 +3,5 @@ pyGATs tis python3 library which combines power of pyautogui, opencv,
 tesseract, markdown and other staff to automate end-to-end and exploratory
 testing.
 """
+
 __all__ = ["formatters", "pygats", "misc", "recog"]
